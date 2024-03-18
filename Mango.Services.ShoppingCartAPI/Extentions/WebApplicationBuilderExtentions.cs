ee3e6a53c282be3974d008f310021db561d9694b
eslam148 <Islam.18333388@compit.aun.edu.eg>
2024-03-19 00:43:58 +0200

    Start Shopping Cart

---
+++ Mango.Services.ShoppingCartAPI/Extentions/WebApplicationBuilderExtentions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Mango.Services.ShoppingCartAPI.Extentions
+{
+    public static class WebApplicationBuilderExtentions
+    {
+        public static WebApplicationBuilder AddAppAuthentication(this WebApplicationBuilder builder) {
+             var settingSection = builder.Configuration.GetSection("ApiSettings");
+            var secret = settingSection.GetValue<string>("Secret");
+            var Audience = settingSection.GetValue<string>("Audience");
+            var Issuer = settingSection.GetValue<string>("Issuer");
+
+            var Key = Encoding.ASCII.GetBytes(secret);
+
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Key),
+                    ValidateIssuer = true,
+                    ValidIssuer = Issuer,
+                    ValidAudience = Audience,
+                    ValidateAudience = true,
+                };
+            });
+
+            return builder;
+        }
+    }
+}
